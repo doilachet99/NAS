7db0727a9d4e06fbf665966ff341a19195b44cae
doilachet99 <doilachet99@gmail.com>
2023-08-08 16:30:46 +0700

    add funtion
    add interface and code

---
+++ NAS/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace NAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.IPAddressTextBox = new System.Windows.Forms.TextBox();
+            this.SharedFolderTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.ResultListBox = new System.Windows.Forms.ListBox();
+            this.PortTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(12, 279);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 0;
+            this.ConnectButton.Text = "button1";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // IPAddressTextBox
+            // 
+            this.IPAddressTextBox.Location = new System.Drawing.Point(12, 12);
+            this.IPAddressTextBox.Name = "IPAddressTextBox";
+            this.IPAddressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.IPAddressTextBox.TabIndex = 1;
+            // 
+            // SharedFolderTextBox
+            // 
+            this.SharedFolderTextBox.Location = new System.Drawing.Point(12, 38);
+            this.SharedFolderTextBox.Name = "SharedFolderTextBox";
+            this.SharedFolderTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SharedFolderTextBox.TabIndex = 1;
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(12, 88);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UsernameTextBox.TabIndex = 1;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(12, 114);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordTextBox.TabIndex = 1;
+            // 
+            // ResultListBox
+            // 
+            this.ResultListBox.FormattingEnabled = true;
+            this.ResultListBox.Location = new System.Drawing.Point(12, 155);
+            this.ResultListBox.Name = "ResultListBox";
+            this.ResultListBox.Size = new System.Drawing.Size(776, 95);
+            this.ResultListBox.TabIndex = 2;
+            // 
+            // PortTextBox
+            // 
+            this.PortTextBox.Location = new System.Drawing.Point(12, 62);
+            this.PortTextBox.Name = "PortTextBox";
+            this.PortTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PortTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultListBox);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.PortTextBox);
+            this.Controls.Add(this.SharedFolderTextBox);
+            this.Controls.Add(this.IPAddressTextBox);
+            this.Controls.Add(this.ConnectButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox IPAddressTextBox;
+        private System.Windows.Forms.TextBox SharedFolderTextBox;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.ListBox ResultListBox;
+        private System.Windows.Forms.TextBox PortTextBox;
+    }
+}
+
---
+++ NAS/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpCifs.Smb;
+
+namespace NAS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            string ipAddress = IPAddressTextBox.Text;
+            string sharedFolder = SharedFolderTextBox.Text;
+            string port = PortTextBox.Text;
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+
+            try
+            {
+                string fullPath = $"smb://{ipAddress}:{port}/{sharedFolder}";
+
+                SmbFile smbFile = new SmbFile(fullPath, new NtlmPasswordAuthentication(ipAddress, username, password));
+
+                if (smbFile.Exists())
+                {
+                    SmbFile[] files = smbFile.ListFiles();
+                    foreach (SmbFile file in files)
+                    {
+                        ResultListBox.Items.Add(file.GetName());
+                    }
+                }
+                else
+                {
+                    ResultListBox.Items.Add("Thư mục không tồn tại hoặc không có quyền truy cập.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultListBox.Items.Add("Lỗi: " + ex.Message);
+            }
+
+
+
+
+
+        }
+
+    }
+}
